b6eea4c21e38694330f6be0f8ee297fa5fbbd7ae
erufeil <eduardo.rufeil@gmail.com>
2024-12-19 15:30:36 -0300

    Modificaciones para convertirlo a ES6

--- src/config/env.js
+++ src/config/env.js
@@ -1,11 +1,15 @@
-require("dotenv").config();
-const { get } = require("env-var");
+//require("dotenv").config();
+import env from "dotenv";
+env.config();
+//const { get } = require("env-var");
+import envvar from "env-var";
 
-const envs = {
-    PORT: get("PORT").required().asPortNumber(),
-    PUBLIC_PATH: get("PUBLIC_PATH").default("public").asString()
+
+export const envs = {
+    PORT: envvar.get("PORT").required().asPortNumber(),
+    PUBLIC_PATH: envvar.get("PUBLIC_PATH").default("public").asString()
 }
 
-module.exports = {
-    envs
-}
+//module.exports = {
+//    envs
+//}
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,9 @@
-const express = require("express")
-const path = require("path")
+//const express = require("express")
+import express from "express"
+//const path = require("path")
+import path from "path"
 
-const startServer = (options) => {
+export const startServer = (options) => {
     const { port, public_path = "public" } = options
     console.log(port)
     console.log(public_path)
@@ -22,6 +24,6 @@ const startServer = (options) => {
 }
 
 
-module.exports = {
-    startServer
-}
+//module.exports = {
+//    startServer
+//}
